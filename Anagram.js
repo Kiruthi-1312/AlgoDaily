167456d044c90cff4adc6a37a37c013c33453d45
Kiruthika <66116348+Kiruthi-1312@users.noreply.github.com>
2021-03-12 21:58:06 +0530

    Create Anagram.js

---
+++ Anagram.js
@@ -0,0 +1,19 @@
+//We are given two strings like "cinema" and "iceman" as inputs. 
+//Can you write a method isAnagram(str1, str2) that will return true or false depending on whether the strings are anagrams of each other?
+
+
+
+function isAnagram(str1,str2)
+{
+   let a = str1.toLowerCase();
+   let b = str2.toLowerCase();
+
+   a = a.split("").sort().join("");
+   b = b.split("").sort().join("");
+
+
+   return a===b;
+}
+
+console.log(isAnagram("army","mary"));
+console.log(isAnagram("icecream","ice"))
